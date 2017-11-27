8d521356078c85f4bea299039f63a0f6e02fe713
Hoang Phan <phan.minhhoang2606@gmail.com>
2017-11-27 14:51:48 -0500

    Increase word data base.

--- vietdict.js
+++ vietdict.js
@@ -3,7 +3,9 @@ function getMap(){
 	var ifrm = document.getElementById("ifrm");
 	var frameDoc = ifrm.contentDocument || iframe.contentWindow.document;
 	var data = frameDoc.getElementsByTagName('text')[0].innerHTML;
-    var wordList = data.split('\n');    for (var i=1;i<wordList.length;i++){
+    var wordList = data.split('\n').join(';').split(' ').join(';').split(';');
+    console.log(wordList);
+    for (var i=1;i<wordList.length;i++){
     	input = wordList[i];
 		input = removeDialect(input);
 		input = input.split(' ').join('');
